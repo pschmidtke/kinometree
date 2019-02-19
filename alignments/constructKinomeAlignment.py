6758b745339f890354e67c1f22096c292adc0cfd
Peter Schmidtke <peter.schmidtke@discngine.com>
2019-02-19 17:09:14 +0100

    traindevelopment

--- alignments/constructKinomeAlignment.py
+++ alignments/constructKinomeAlignment.py
@@ -1,6 +1,10 @@
 import prody.sequence as sequence
 import csv 
 import sys
+import prody
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import generic_protein
 sys.path.insert(0, '/Users/peter/Documents/Work/3decision/3decision_python')
 
 import db_interface as dbi3dec
@@ -9,17 +13,55 @@ input_file = csv.DictReader(open("kinome.csv"),delimiter=';' )
 
 #TODO : check why a few uniprot codes are missing here
 
+domain_annotations=['Protein kinase domain (Domain)','Protein kinase domain (Domain)']
+expected_pfam_families=['','']
+
 o3i=dbi3dec.Oracle3decInterface()
 o3i.connect()
 for line in input_file:
     uniprot_code=(line['UNIPROT_CODE'])
     if(len(uniprot_code)):
-        sequence=o3i.getCanonicalSequenceFromUniprotCode(uniprot_code)
+        ### get the uniprot canonical sequence from the database (full kinase sequence)
+        raw_sequence=o3i.getCanonicalSequenceFromUniprotCode(uniprot_code)
+        uniprot_sequence=Seq(raw_sequence, generic_protein)
+
         seq_id=o3i.getCanonicalSeqIdFromUniprotCode(uniprot_code)
-        seqSpan=o3i.getSeqSpanFromAnnotation( seq_id,'Protein kinase domain (Domain)')
-        print(seqSpan)
-        #print(seqEnd-seqStart)
+
+        # get a list of start and end positions for a given annotation on the sequence
+        # if none is returned, then the annotation is not found for this particular sequence
+        cur_domain_idx=1
+
+        #first let's get all domain annotations from PFAM, if available
+        # 
+        pfam_annotations=prody.searchPfam(uniprot_code)
+        pfam_families=pfam_annotations.keys()
+        
+
+        for annotation in domain_annotations:
+                seqSpans=o3i.getSeqSpanFromAnnotation( seq_id,annotation)
+                if len(seqSpans):       #check if None maybe? 
+                        ### for each domain definition in a uniprot sequence (there might be multiple)
+                        ### extract the sequence of the domain itself
+                        for seqSpan in seqSpans:
+                                startPosition=seqSpan[0]-1
+                                endPosition=seqSpan[1]
+                                if  (endPosition-startPosition) >200 and (endPosition-startPosition)<500:
+                                        domain_seq=Seq(uniprot_sequence[startPosition:endPosition],generic_protein)
+                                        domain_seq_record=SeqRecord(domain_seq,id=uniprot_code+".".str(cur_domain_idx))
+                                else :
+                                        print("warning, annotation yielded sequence too short or too long for "+uniprot_code)
+                                        cur_domain_idx+=1
+
+                                #TODO: write into fasta file for alignment
+
+                        #print(seqEnd-seqStart)
 
 
-
+#TODO:
+#do alignment
+#spot issues within alignment (vs reference sequences with ref annotations)
+#correct issues in DB
+#correct issues in alignment?
+#for each pocket extract relevant pocket residues using a reference structure / pocket? 
+#--> contact based again here?
 
