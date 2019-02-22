3ebb52351fb90beb4b82bb6c11e273a2fb8ce65e
Peter Schmidtke <peter@Mac-Pro.local>
2019-02-22 22:45:10 +0100

    minor changes

--- alignments/constructKinomeAlignment.py
+++ alignments/constructKinomeAlignment.py
@@ -14,7 +14,7 @@ input_file = csv.DictReader(open("kinome.csv"),delimiter=';' )
 #TODO : check why a few uniprot codes are missing here
 
 domain_annotations=['Protein kinase domain (Domain)','Protein kinase domain (Domain)']
-expected_pfam_families=['','']
+expected_pfam_families=['PF00069','PF07714'] #,'PF12330','PF00454','PF01163','PF03109']
 
 o3i=dbi3dec.Oracle3decInterface()
 o3i.connect()
@@ -35,22 +35,28 @@ for line in input_file:
         # 
         pfam_annotations=prody.searchPfam(uniprot_code)
         pfam_families=pfam_annotations.keys()
-        
+        print(pfam_families)
+        for pfam_family in pfam_families:
+                if pfam_family in expected_pfam_families:
+                        print(pfam_annotations[pfam_family])
 
         for annotation in domain_annotations:
                 seqSpans=o3i.getSeqSpanFromAnnotation( seq_id,annotation)
                 if len(seqSpans):       #check if None maybe? 
                         ### for each domain definition in a uniprot sequence (there might be multiple)
                         ### extract the sequence of the domain itself
-                        for seqSpan in seqSpans:
+                        if(len(seqSpans)==2):
                                 startPosition=seqSpan[0]-1
                                 endPosition=seqSpan[1]
-                                if  (endPosition-startPosition) >200 and (endPosition-startPosition)<500:
-                                        domain_seq=Seq(uniprot_sequence[startPosition:endPosition],generic_protein)
-                                        domain_seq_record=SeqRecord(domain_seq,id=uniprot_code+".".str(cur_domain_idx))
-                                else :
-                                        print("warning, annotation yielded sequence too short or too long for "+uniprot_code)
-                                        cur_domain_idx+=1
+                        else : 
+                                for seqSpan in seqSpans:
+                                        
+                                        if  (endPosition-startPosition) >200 and (endPosition-startPosition)<500:
+                                                domain_seq=Seq(uniprot_sequence[startPosition:endPosition],generic_protein)
+                                                domain_seq_record=SeqRecord(domain_seq,id=uniprot_code+".".str(cur_domain_idx))
+                                        else :
+                                                print("warning, annotation yielded sequence too short or too long for "+uniprot_code)
+                                                cur_domain_idx+=1
 
                                 #TODO: write into fasta file for alignment
 
