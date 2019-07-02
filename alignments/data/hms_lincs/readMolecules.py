796cd4df400e09babf8b72c2b51fd5102be8c75c
pschmidtke <peter.schmidtke@discngine.com>
2019-07-02 20:31:07 +0200

    integration of 3dec rest API

--- alignments/data/hms_lincs/readMolecules.py
+++ alignments/data/hms_lincs/readMolecules.py
@@ -1,13 +1,46 @@
 import glob
 import csv
 import pandas as pd
-files=glob.glob("*_molecule.csv")
-for file in files:
-    try: 
-        x=pd.read_csv(file,sep=",",quoting=csv.QUOTE_ALL)
+import requests as requests
+from requests import Session
+from requests.auth import HTTPBasicAuth
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import sys
+
+uname=sys.argv[1]
+pwd=sys.argv[2]
+
+s = Session()
+host="https://localhost:9003/api/v2"
+
+response=s.post(host+'/login',data = {'username':uname},verify=False, auth=(uname, pwd))
+if response.status_code==200:
+    files=glob.glob("*_molecule.csv")
+    for file in files:
+        try: 
+            x=pd.read_csv(file,sep=",",quoting=csv.QUOTE_ALL)
+            if(len(x)==1):
+                smiles=x["SMILES"].to_list()[0]
+                smilesResponse=s.get(host+"/ligand/search/similarity/smiles/"+smiles)
+                if smilesResponse.status_code==200:
+                    smilesResponseJson=response.json()
+                    print(smilesResponseJson)
+                    #print(smilesResponseJson["data"])
+
+                else: 
+                    print(smilesResponse.status_code)
+                    print(smilesResponse.text)
+                    print(file,"nothing found")
+
+        except Exception:
+            pass
+
+else:
+    print(response.status_code)
+    print(response.text)
         
-        if(len(x)==1):
-            print(file,x["Name"].to_list()[0],x["SMILES"].to_list()[0])
-    except Exception:
-        pass
+
+
+
     
